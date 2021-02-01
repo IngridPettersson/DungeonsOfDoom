ad66b8fea2b2e3de795b31a32e31c9d58e01a867
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-02-01 15:52:26 +0100

    Add change

---
+++ DungeonsOfDoom/Character.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonsOfDoom
+{
+    class Character
+    {
+        public int Health { get; set; }
+        public int Damage { get; set; }
+        public string Name { get; set; }
+    }
+}
--- DungeonsOfDoom/Program.cs
+++ DungeonsOfDoom/Program.cs
@@ -3,10 +3,17 @@
 namespace DungeonsOfDoom
 {
 
+    // Skapa en basklass som innehåller allt som är gemensamt mellan spelare och monster.
+    // Implementera ett till arv, tex två subklasser till Monster eller Item.
+    // Se till att era klasser har lämpliga constructors.
+    // Se till att alla klasser som uteslutande ska ärvas från är markerade som abstrakta.
+
+    // Om tid finns: lägg till ett par egna properties för att göra spelet roligare :)
+
     // Världen skapas en gång (CreateWorld())
     // Inväntar ett movement från användaren, alltså ett tangenttryck (Console.ReadKey()).
     // if statement efter switch statement när man tryckt på tangent, kontrollerar så att newX och newY är över 0 och mindre än world (Room[,]) längd då respektive axel.
-    // spelplanen ritas upp efter varje knapptryck
+    // spelplanen ritas upp efter varje tangenttryck
     // health blir 0, då avslutas spelet (do while loop i ConsoleGame)
     class Program
     {
