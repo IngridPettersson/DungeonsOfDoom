7fc890f9776c324d12891ea427d38700fe82228b
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-02-01 15:21:30 +0100

    Add changes

--- DungeonsOfDoom/Program.cs
+++ DungeonsOfDoom/Program.cs
@@ -5,9 +5,9 @@ namespace DungeonsOfDoom
 
     // Världen skapas en gång (CreateWorld())
     // -
-    // if statement efter switch statement när man tryckt på knapp.
-    // spelplanen ritas upp efter varje förflyttning (eller efter varje knapptryck?)
-    // health blir 0, då avslutas spelet
+    // if statement efter switch statement när man tryckt på knapp, kontrollerar så att newX och newY är över 0 och mindre än world (Room[,]) längd då respektive axel.
+    // spelplanen ritas upp efter varje knapptryck
+    // health blir 0, då avslutas spelet (do while loop i ConsoleGame)
     class Program
     {
         static void Main(string[] args)
